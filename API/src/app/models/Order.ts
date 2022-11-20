bd84fa441f2c2b89db869ce0c94b67f80bcf667a
Michelle Setúbal <missetubal@outlook.com>
2022-11-20 15:59:58 -0300

    creating models

---
+++ API/src/app/models/Order.ts
@@ -0,0 +1,41 @@
+import { model, Schema } from "mongoose";
+import { Category } from "./Category";
+
+export const Order = model(
+  "Order",
+  new Schema({
+    name: {
+      type: String,
+      required: true,
+    },
+    table: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ["WAITING", "IN_PRODUCTION", "DONE"],
+      default: "WAITING",
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    products: {
+      required: true,
+      type: [
+        {
+          product: {
+            type: Schema.Types.ObjectId,
+            required: true,
+            ref: "Product",
+          },
+          quantity: {
+            type: Number,
+            default: 1,
+          },
+        },
+      ],
+    },
+  })
+);
